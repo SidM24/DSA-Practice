6a956f1056fce296e65291f3cf49926b01255428
Siddharth Maithani <siddharthmaithani94@gmail.com>
2022-11-18 14:58:28 +0530

    Added All heap functions

--- C++ Programs/Heaps/heap.cpp
+++ C++ Programs/Heaps/heap.cpp
@@ -63,13 +63,76 @@ void heapsort(int arr[], int n)
     }
 }
 
+// To extract the maximum element of the max-heap
+int heapmaxextract(int arr[], int &n)
+{
+    if (n < 1)
+    {
+        cout << "The heap is empty" << endl;
+    }
+    else
+    {
+        int max = arr[0];
+        swap(arr[0], arr[n - 1]);
+        n = n - 1;
+        heapify(arr, n, 0);
+        return max;
+    }
+}
+
+//Parent function return the index of the parent of the current node
+int parent(int i)
+{
+    return (i - 1) / 2;
+}
+
+// Heap increase the key
+void heapincrease(int arr[], int n, int key, int i)
+{
+    if (key < arr[i])
+    {
+        cout << "Error the increase value should be greater than the original value" << endl;
+        return;
+    }
+    else
+    {
+        arr[i] = key;
+        while (i != 0 && arr[parent(i)] < arr[i])
+        {
+            swap(arr[i], arr[parent(i)]);
+            i = parent(i);
+        }
+    }
+}
+
 // Driver Main
 int main()
 {
     int arr[] = {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
     int N = sizeof(arr) / sizeof(arr[0]);
-    // buildheap(arr, N);
-    heapsort(arr,N);
+    buildheap(arr, N);
+    printeheap(arr, N);
+
+    heapincrease(arr, N, 20, 4);
     printeheap(arr, N);
     return 0;
 }
+
+// heapsort(arr,N);
+// cout<<heapmaxextract(arr, N)<<endl;
+
+//Insert a new key function to the heap
+/*
+void insert(int key)
+{
+    heapsize++;
+    int i=heapsize-1;
+    arr[i]=key;
+    //relocating the new value to its correct location
+    while (i!=0&&parent(i)<arr[i])
+    {
+        swap(arr[i],arr[parent(i)]);
+        i=parent(i);
+    }
+}
+*/ 
