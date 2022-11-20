7e5822d5189d084b9d61e3f236c52b9e825e8914
Siddharth Maithani <siddharthmaithani94@gmail.com>
2022-11-21 05:09:46 +0530

    Added Round Robin and FCFS Codes to OS folder

--- OperatingSystem/FCFS.cpp
+++ OperatingSystem/FCFS.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 int main()
 {
+    // n stores the number of processes
+    int n = 5;
+
     int process[] = {1, 2, 3, 4, 5};
     int arrival_time[] = {2, 5, 1, 0, 4};
     int burst_time[] = {6, 2, 8, 3, 4};
     int tat[] = {0, 0, 0, 0, 0};
     int waiting_time[] = {0, 0, 0, 0, 0};
     int exit_time[] = {0, 0, 0, 0, 0};
-    // n stores the number of processes
-    int n = 5;
+
     // Sorting all the processes according to the arrival times
     // As FCFS is based on the arrival time of different processes
+
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = 0; j < n - i - 1; j++)
@@ -25,7 +28,7 @@ int main()
         }
     }
 
-    // The order in which the process will be executed is:
+    // Order of Execution
     for (int i = 0; i < n; i++)
     {
         cout << process[i] << " ";
@@ -35,7 +38,6 @@ int main()
     // Time counter to keep track of the total time
     int time = 0;
 
-    // Calculating the exit time for each and every process
     for (int i = 0; i < n; i++)
     {
         // Check if for eg the time is 4 and next process arrives at 9 so we need to update time from 4 to 9
@@ -43,21 +45,18 @@ int main()
         {
             time = arrival_time[i];
         }
+
         // ExitTime
         exit_time[i] = time + burst_time[i];
+
         // Updating the time variable
         time = time + burst_time[i];
     }
 
-    // Calculating the TurnAroundTime
+    // Calculating the TurnAroundTime and Waiting Time
     for (int i = 0; i < n; i++)
     {
         tat[i] = exit_time[i] - arrival_time[i];
-    }
-
-    // Waiting Time
-    for (int i = 0; i < n; i++)
-    {
         waiting_time[i] = tat[i] - burst_time[i];
     }
 
